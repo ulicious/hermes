c17eaaf189b8af053bf8e0268314a007a4ea45e6
ulicious <56553461+ulicious@users.noreply.github.com>
2024-10-08 10:49:02 +0200

    Update

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'English'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
