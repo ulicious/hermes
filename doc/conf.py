e42dc71d837393b8552e2c3714ecd3bfacf43b0c
ulicious <56553461+ulicious@users.noreply.github.com>
2025-04-09 13:36:28 +0200

    include logo

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,8 @@ html_logo = "images/logo_final.png"
 html_theme_options = {
     'body_max_width': "none",
     'page_width': '80%',
-    'sidebar_width': '20%'
+    'sidebar_width': '20%',
+    "repository_url": "https://github.com/ulicious/hermes",
 }
 
 html_css_files = [
